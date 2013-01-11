4376de8f3643eea73db1b147a15ecc2e73a85db1
= <luke.metz@students.olin.edu>
2013-01-11 21:49:05 +0000

    first commit

---
+++ download.lua
@@ -0,0 +1,32 @@
+
+local os = require("os")
+local lfs = require("lfs")
+
+local function git_clone(name)
+  os.execute("git clone " .. name)
+end
+
+local function download_file(name)
+  os.execute("wget " .. name)
+end
+
+--install pathogen
+pathogen_file = "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
+
+lfs.chdir("autoload")
+download_file(pathogen_file)
+lfs.chdir("../")
+
+bundles_git = {
+  "https://github.com/Lokaltog/vim-powerline.git",
+  "https://github.com/tpope/vim-surround.git",
+  "https://github.com/tomtom/tcomment_vim.git",
+  "https://github.com/tomasr/molokai.git",
+}
+
+lfs.chdir("bundle")
+for _,name in ipairs(bundles_git) do
+  git_clone(name) 
+end
+lfs.chdir("../")
+
