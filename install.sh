53d3092fafd6e64aa3c9b36c5130bc3674047f9d
MattWis <matthew.wismer@students.olin.edu>
2014-08-23 18:41:00 -0400

    Bug fix in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
-rm ~/.vimrc
-rm ~/.gvimrc
+# Squash "No such file" errors
+rm ~/.vimrc &> /dev/null
+rm ~/.gvimrc &> /dev/null
+rm ~/.neovimrc &> /dev/null
+rm ~/.neovim &> /dev/null
+
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
-
-rm ../.neovimrc
 ln -s ~/.vim/vimrc ~/.neovimrc
+
 ln -s ~/.vim ~/.neovim
 
 lua download.lua
