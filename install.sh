66638ffbb1ad81fc81aadfce2084b5c6ff1c9f81
MattWis <matthew.wismer@students.olin.edu>
2014-08-27 14:45:49 -0400

    Correct filenames for neovim support

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 # Squash "No such file" errors
 rm ~/.vimrc &> /dev/null
 rm ~/.gvimrc &> /dev/null
-rm ~/.neovimrc &> /dev/null
-rm ~/.neovim &> /dev/null
+rm ~/.nvimrc &> /dev/null
+rm ~/.nvim &> /dev/null
 
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
-ln -s ~/.vim/vimrc ~/.neovimrc
-
-ln -s ~/.vim ~/.neovim
+ln -s ~/.vim/vimrc ~/.nvimrc
+ln -s ~/.vim ~/.nvim
 
 lua download.lua
